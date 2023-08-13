124ab2130e5a4e8c90fb882dbd38459a9fae6ba7
James Campbell <james@tunerpreferred.com>
2023-08-12 23:11:41 -0400

    Css is painful

---
+++ ui/lifttrackgain/src/components/MNavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MNavbar = () => {
+    return (
+        <div className="text-white">
+            <h1 className="w-full">React.</h1>
+        </div>
+    )
+}
+
+export default MNavBar
