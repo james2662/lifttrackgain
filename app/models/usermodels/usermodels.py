19caea018f03cc9614e39384b87c5bfb9ef48cec
James Campbell <james@tunerpreferred.com>
2024-04-05 10:56:24 -0400

    Building out user related models and shemas

--- app/models/usermodels/usermodels.py
+++ app/models/usermodels/usermodels.py
@@ -1,5 +1,26 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional
 
+# Schemeas 
 class UserBase(SQLModel):
-    username: str = Field(index=True)
-    useremail: str = Field(index=True)
+    username: str = Field(index=True, unique=True, nullable=False)
+    useremail: str = Field(index=True, nullable=False)
+    is_active: bool = Field(default=True)
+
+class UserCore(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True, index=True)
+    hashed_password: str = Field(nullable=False)
+
+# Response and Request Models
+class ltgUserBase(SQLModel):
+    username: str
+    user_email: str
+
+class ltgCreatedUser(ltgUserBase):
+    password: str
+
+class ltgCreateUserReqeust(ltgUserBase)
+    password: str
+    confirm_password: str
+
+
